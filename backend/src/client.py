d2f6a159e9b4ef2cb647b21b02d7628331cc8d4d
tobiaszrolla <154612005+tobiaszrolla@users.noreply.github.com>
2025-03-23 00:03:52 +0100

    Feature/tasks (#5)
    * 22.03.2025 14:20
    Komunikacja klient serwer
    requiraments request
    Należy to poprawić
    
    * 22.03.2025 16:20
    Zmieniono klienta i serwer aby rozwiązywały zadania
    TASK: Trzeba dodać weryfikacje klienta aby zadanie nie było pułblicznie widoczne
    
    ---------
    
    Co-authored-by: Adam Makarewicz <adam.makarewicz@proton.me>

--- backend/src/client.py
+++ backend/src/client.py
@@ -5,8 +5,24 @@ BASE_URL = "http://127.0.0.1:8080"
 
 while True:
     try:
-        response = requests.post(f"{BASE_URL}/ping")
+        response = requests.post(f"{BASE_URL}/ping")  # Wysyłamy POST
         print("Server response:", response.json())
+
+        task_response = requests.get(f"{BASE_URL}/task") #Pobranie Zadania
+        task = task_response.json()
+
+        a, b = task["a"], task["b"] #Rozwiązanie Zadania przez klienta
+        print(f"Received task: {a} + {b}")
+        result = {"sum": a + b}
+
+        response = requests.post(f"{BASE_URL}/result", json=result) #Wysłanie Odpowiedzi
+        server_reply = response.json()
+        print("Server response:", server_reply)
+
+
+
+    except requests.exceptions.Timeout:
+        print("Server timeout, retrying...")
     except requests.exceptions.RequestException as e:
         print("Server unreachable:", e)
     time.sleep(5)
