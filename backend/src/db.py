8becca6999a494824bfd93a448986514cdb9d9c5
Tobi <rollatobiasz@gmail.com>
2025-05-07 15:43:34 +0200

    jest baza jest front klienta na razie tylko rejestracja

---
+++ backend/src/db.py
@@ -0,0 +1,10 @@
+# db.py
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "postgresql://admin:pasword@localhost:5432/crypto_mining" #Adres bazy danych
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
---
+++ backend/src/models.py
@@ -0,0 +1,11 @@
+# models.py
+from sqlalchemy import Column, Integer, String
+from db import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String, nullable=False)
+    public_key = Column(String)
