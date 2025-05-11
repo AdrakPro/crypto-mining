1f499d091e163c09bc1b1beb85cc185005b8ecb8
Tobi <rollatobiasz@gmail.com>
2025-05-11 21:18:11 +0200

    pobieranie tego klucza

--- frontend_client/src/register.js
+++ frontend_client/src/register.js
@@ -50,3 +50,23 @@ export async function registerUser(e) {
     msg.style.color = "red";
   }
 }
+window.downloadPrivateKey = function () {
+  const privateKey = document.getElementById("generatedPrivateKey").value;
+
+  if (!privateKey) {
+    alert("Brak klucza prywatnego do pobrania!");
+    return;
+  }
+
+  const blob = new Blob([privateKey], { type: "text/plain;charset=utf-8" });
+  const url = URL.createObjectURL(blob);
+
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "private_key.pem";
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+
+  URL.revokeObjectURL(url);
+};
